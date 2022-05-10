913543cae113f9428ab50bc44e53ffddcb82f67d
Aleksandr Peshkov <a.piashkou@gmail.com>
2022-05-11 00:22:35 +0300

    geometry changed

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
 // FPS
@@ -10,37 +10,41 @@ import * as dat from 'dat.gui'
 const gui = new dat.GUI()
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.getElementById('app')
 
 // Scene
 const scene = new THREE.Scene()
 
+
 // Objects
-const dino = new THREE.SphereGeometry(1, 10, 10)
-const plane = new THREE.PlaneGeometry(20, 20)
+const dino = new THREE.BoxGeometry(1, 1, 1)
+const plane = new THREE.PlaneGeometry(100, 100)
+
 
 // Materials
-const material1 = new THREE.MeshStandardMaterial({
-  color: 0x228b22
+const dinoMaterial = new THREE.MeshPhongMaterial({
+  color: 0x96d35f
 })
 
-const material2 = new THREE.MeshStandardMaterial({
+const groundMaterial = new THREE.MeshPhongMaterial({
   color: 0xfeaf1c
 })
 
-// Mesh
-const robozavr = new THREE.Mesh(dino,material1)
-robozavr.position.set(0,1,0)
 
-const ground = new THREE.Mesh(plane,material2)
+// Mesh
+const robozavr = new THREE.Mesh(dino, dinoMaterial)
+robozavr.position.set(0, 0.5, 0)
+
+const ground = new THREE.Mesh(plane, groundMaterial)
 ground.rotateX(-1.5708)
 
 scene.add(robozavr)
 scene.add(ground)
 
+
 // Lights
-const pointLight = new THREE.PointLight(0xffffff, 2)
-pointLight.position.set(2,4,2)
+const pointLight = new THREE.PointLight(0xffffff, 1)
+pointLight.position.set(0,4,2)
 scene.add(pointLight)
 
 const lightFolder = gui.addFolder('pointLight')
@@ -79,75 +83,70 @@ scene.add( axesHelper );
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
  */
+
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 5
-camera.position.y = 5
-camera.position.z = 5
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 50)
+camera.position.set(0, 2, 5)
 
 scene.add(camera)
 
-// const cameraHelper = new THREE.CameraHelper( camera );
-// scene.add( cameraHelper );
-
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true
+  canvas: canvas
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
 
 /**
  * Animate
  */
-
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
 
-    const elapsedTime = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    robozavr.rotation.y = .5 * elapsedTime
+  // Update objects
+  robozavr.rotation.y = .5 * elapsedTime
 
-    // Update Orbital Controls
-    controls.update()
+  // Update Orbital Controls
+  // controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
